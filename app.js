5dc72b0fafcb15ad380143b5272cbf8bcc7f5025
Saulius Prasmantas <sprasmantas@telesign.com>
2018-03-14 15:20:44 +0200

    Add Prometheus

--- app.js
+++ app.js
@@ -2,22 +2,25 @@ const http = require('http');
 const os = require('os');
 
 port = getEnv("PORT", "8080");
+msg = getEnv("MESSAGE", "Hello DevOpsPro");
 
 console.log("AppX server starting...");
 
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("Hello from " + os.hostname() + "\n");
+  response.end(msg+ " from " + os.hostname() + "\n");
 };
 var www = http.createServer(handler);
 www.listen(port, function (err) {
-  console.log('listening http://localhost:'+port+"/");
+  console.log('listening on http://localhost:'+ port +"/");
   console.log('pid is ' + process.pid);
 });
 
 process.on('SIGTERM', function () {
+  console.log('Terminating AppX')
   server.close(function () {
+
     process.exit(0);
   });
 });
