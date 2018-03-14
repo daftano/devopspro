5ff3465f698baff12e7ee1b924332a373a9b8d7f
Davide Fiorentino lo Regio <davide.fiorentino.loregio@gmail.com>
2018-03-14 12:32:31 +0200

    fix reverse quote

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("AppX server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end(“Hello from " + os.hostname() + "\n");
+  response.end("Hello from " + os.hostname() + "\n");
 };
 var www = http.createServer(handler);
 www.listen(8080);
